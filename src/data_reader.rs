ea4d18c1e655a0f5b3c9062df2433a91dde23933
DanielGall500 <daniel.gallagher.js@gmail.com>
2024-08-29 11:42:24 +0200

    csv reader impl

---
+++ src/data_reader.rs
@@ -0,0 +1,42 @@
+use csv::{self};
+use serde::Deserialize;
+use std::error::Error;
+
+fn read_csv(path: &str) -> Result<Vec<Record>, Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .delimiter(b',')
+        .escape(Some(b'\\'))
+        .from_path(path)?;
+
+    let mut records = Vec::new();
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        records.push(record);
+    }
+    Ok(records)
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Record {
+    pub phonetic_rep: String,
+    pub english_translation: String,
+    pub pos: String,
+    pub source: Option<String>,
+    pub structure: String,
+}
+
+pub struct Dataset {
+    pub path: String,
+    pub data: Vec<Record>,
+}
+
+impl Dataset {
+    pub fn new(path: String) -> Result<Dataset, Box<dyn Error>> {
+        let data = read_csv(&path)?;
+        Ok(Self {
+            path: path,
+            data: data,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 pub mod pw;
+pub mod data_reader;
+use std::{
+    process::exit,
+    error::Error,
+};
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let data_path = String::from("data/dagbani-dataset.csv");
+    let dataset = data_reader::Dataset::new(data_path)?;
+    for word in &dataset.data {
+        println!("{}", word.pos);
+    }
+    Ok(())
+}
 
 fn main() {
     println!("Hello, world!");
     let example = String::from("Hello!");
     let word_example = pw::PhonologicalWord::new(example);
     word_example.get_cv_structure();
+
+    if let Err(err) = run() {
+        println!("{}", err);
+        exit(1);
+    }
+
+    
 }
