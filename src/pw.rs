548f72202e7586d8c686428b0ff16559aafec9dd
unknown <daniel.gallagher.js@gmail.com>
2024-09-20 15:34:18 +0100

    advanced CV structure method impl

--- src/pw.rs
+++ src/pw.rs
@@ -38,7 +38,41 @@ impl PhonologicalWord {
         vowels
     }
 
-    pub fn get_cv_structure(&self) -> String {
+    // TODO: consonant cluster impl, e.g CVCCCV 
+    pub fn get_cv_structure(&self) -> Vec<&str> {
+        let decomposed_word = remove_diacritics(&self.phon_word);
+
+        let mut cv_structure: Vec<&str> = Vec::new();
+        let mut is_syllable_closed = true;
+
+        let cv_simple: String = self.get_cv_structure_simple();
+        let n_vowels_left: usize = self.get_vowels().chars().count();
+        let mut n_vowels_left: i8 = n_vowels_left as i8;
+
+        for (i, phon) in decomposed_word.chars().enumerate() {
+            if self.consonants.contains(&phon) {
+                if i + 2 <= cv_simple.len() && &cv_simple[i..i+2] == "CV" {
+                    cv_structure.push("C_Onset"); 
+                    is_syllable_closed = false;
+                } else if !is_syllable_closed { // we must close the syllable before moving on
+                    cv_structure.push("C_Coda"); 
+                    is_syllable_closed = true;
+                } else if n_vowels_left >= 1 { // there is another syllable to be parsed
+                    cv_structure.push("C_Onset"); 
+                    is_syllable_closed = false;
+                }
+            } 
+            else {
+                cv_structure.push("V"); 
+                is_syllable_closed = false;
+                n_vowels_left -= 1;
+            }
+        }
+
+        cv_structure
+    }
+
+    pub fn get_cv_structure_simple(&self) -> String {
         let decomposed_word = remove_diacritics(&self.phon_word);
 
         let mut cv_structure: String = String::new();
@@ -54,7 +88,7 @@ impl PhonologicalWord {
     }
 
     pub fn get_prosodic_position(&self) -> String {
-        let cv_structure: String = self.get_cv_structure();
+        let cv_structure: String = self.get_cv_structure_simple();
         if cv_structure == "CV" {
             String::from("Free-Standing CV")
         }
